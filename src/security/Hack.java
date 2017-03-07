4bcda8a2c0d3f772934d63b33fa4fe45392a2721
xinfu <xin.1.fu@nokia.com>
2017-03-07 16:08:51 +0800

    add class loader

--- src/security/Hack.java
+++ src/security/Hack.java
@@ -9,6 +9,9 @@
  */
 package security;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * TODO:Write class description
  * @author <a HREF="mailto:xin.1.fu@nokia.com">Fu Xin</a>
@@ -16,10 +19,30 @@ package security;
  */
 public class Hack
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        SecurityManager m = System.getSecurityManager();
-        m.checkPermission( new SaPermission( "go" ) );
-        System.out.println( 1 );
+//        SecurityManager m = System.getSecurityManager();
+//        m.checkPermission( new SaPermission( "go" ) );
+        SaLoader loader = new SaLoader( new URL[]{ new URL( "file:/D:/git/Sacephor/bin" ) } );
+        SaLoader loader2 = new SaLoader( new URL[]{ new URL( "file:/D:/git/Sacephor/bin" ) } );
+        Class<?> h = loader.loadClass( "security.Hack" );
+        Class<?> h2 = loader2.loadClass( "security.Hack" );
+        System.out.println( h );
+        System.out.println( h2.equals( h ) );
+        loader.close();
+        loader2.close();
     }
+}
+
+class SaLoader extends URLClassLoader
+{
+
+    /**
+     * @param urls
+     */
+    public SaLoader( URL[] urls )
+    {
+        super( urls );
+    }
+
 }
